3c99e72ce839d0bd909b0962e84a96165cbab630
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-07-17 20:16:37 -0300

    Add items icons

--- pages/TodoCard.tsx
+++ pages/TodoCard.tsx
@@ -1,14 +1,23 @@
 import { NextPage } from "next"
 import { Todo } from "./interfaces"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faToggleOn, faToggleOff, faPenToSquare } from "@fortawesome/free-solid-svg-icons"
 
 
 const TodoCard: NextPage<{ todo: Todo }> = ({ todo }) => {
-    return <div className="card">
-      <li>{todo.id}</li>
-      <li>{todo.completed? 'True': 'False'}</li>
-      <li>{todo.title}</li>
+  const completedClass = todo.completed ? 'border-green-700' : 'border-red-800'
+  const toggleIcon = todo.completed ? faToggleOn : faToggleOff
+
+  return (
+    <div className={`${completedClass} card border-solid border-b-4`}>
+      <div className="">
+        <FontAwesomeIcon icon={toggleIcon} className='w-7 h-7 inline' />
+        <FontAwesomeIcon icon={faPenToSquare} className='w-6 h-6 inline ml-2 mr-2' />
+        <span className="capitalize">{todo.title}</span>
+      </div>
     </div>
-  
-  }
+  )
+
+}
 
 export default TodoCard
