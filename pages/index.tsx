66f0f9ef24e7a89825e2f536a3a72496b341c577
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-07-17 11:41:30 -0300

    Chore: add basics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,50 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+
+interface Todo {
+  userId: number,
+  id: number,
+  title: string,
+  completed: boolean
+}
+
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string,
+  phone: string
+  website: string
+  company: { name: string, catchPhrase: string, bs: string }
+  address: {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: { lat: number, lng: number }
+  }
+}
+
+
+const UserCard: NextPage<{ user: User }> = ({ user }) => {
+  return <div className="w-full">{user.name}</div>
+
+}
+
+const TodoCard: NextPage<{ todo: Todo }> = ({ todo }) => {
+  return <div className="w-full">
+    <li>{todo.id}</li>
+    <li>{todo.completed}</li>
+    <li>{todo.title}</li>
+  </div>
+
+}
+
+const Home: NextPage<{ users: User[], todos: Todo[] }> = ({ users, todos }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,61 +53,38 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="columns-3">
+          {users.map(u => <UserCard key={u.id} user={u} />)}
+        </div>
+        <div className="columns-3">
+          {todos.map(t => <TodoCard key={t.id} todo={t} />)}
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
 
 export default Home
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const todos: Todo[] = await (await fetch('https://jsonplaceholder.typicode.com/todos')).json()
+  const users: User[] = await (await fetch('https://jsonplaceholder.typicode.com/users')).json()
+
+
+  return {
+    props: {
+      users,
+      todos
+    }
+  }
+}
